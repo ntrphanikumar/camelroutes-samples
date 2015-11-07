16b38b6f2740f72bdb8eae6ade6f13d680f7369d
N T R Phani Kumar <pkumar-consultant@quixey.com>
2015-11-06 17:32:57 -0800

    Seda tester with timeout

---
+++ src/main/java/learn/quixey/camelroute/component/SedaRoute.java
@@ -0,0 +1,40 @@
+package learn.quixey.camelroute.component;
+
+import org.apache.camel.ExchangeTimedOutException;
+import org.apache.camel.WaitForTaskToComplete;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.main.Main;
+
+public class SedaRoute extends RouteBuilder {
+    private static final String SEDA_WAIT_TEST = "seda:waittest";
+
+    @Override
+    public void configure() throws Exception {
+        int timeout = 3000, sleepTime = 1000;
+        onException(ExchangeTimedOutException.class).process(exchange -> {log.error("Seda I am upset with your response time !!!! pchhh");}).handled(true);
+        
+        from("timer://sedatester?repeatCount=1")
+            .to(SEDA_WAIT_TEST + "?timeout=" + timeout + "&waitForTaskToComplete=" + WaitForTaskToComplete.Always)
+            .process(exchange -> {
+                log.info("Hurray!!!! Seda responded... Let me see what he has got for me ..");
+                log.info(exchange.getIn().getBody(String.class));
+            });
+        
+         from(SEDA_WAIT_TEST)
+             .process(exchange -> sleepFor(sleepTime))
+             .setBody(constant("Hmmm waited for a long time through !!!"));
+    }
+    
+    private void sleepFor(long timeInMillis) throws InterruptedException {
+        log.info("Feeling sleepy... Let me sleep before going back for millis: " + timeInMillis);
+        Thread.sleep(timeInMillis);
+        log.info("Hawwww... I am back.. Hope still seda is waiting for me");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.enableHangupSupport();
+        main.addRouteBuilder(new SedaRoute());
+        main.run();
+    }
+}
